25e9e487fd6e5d9391400ce0c1cdd2abadba6848
Jam Risser <jam@codejam.ninja>
2019-04-23 22:11:04 -0500

    Updated user data script

--- aws/user-data.sh
+++ aws/user-data.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-yum update
+
+export PUBLIC_IPV4=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+
+sudo yum update -y
+sudo yum install -y freeipa-server ipa-server-dns
+HOSTNAME="$PUBLIC_IPV4 ${name}.${domain} ${name}.${domain}"
+sudo sed -i "1s/^/$HOSTNAME\n/" /etc/hosts
+sudo ipa-server-install \
+     --auto-forwarders \
+     --auto-reverse \
+     --hostname ${name}.${domain} \
+     --setup-dns \
+     -U \
+     -a password -p password \
+     -n ${name}.${domain} \
+     -r ${upper(name)}.${upper(domain)}
